32eed0b162ebbec0ca8e1456be242b3bbbd13360
Fuzi18 <fuzesi.t2003@gmail.com>
2023-03-30 19:30:48 +0200

    shopp

--- shop.sql
+++ shop.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2023. Már 30. 16:31
--- Kiszolgáló verziója: 10.4.24-MariaDB
--- PHP verzió: 8.1.6
+-- Létrehozás ideje: 2023. Már 30. 19:30
+-- Kiszolgáló verziója: 10.4.18-MariaDB
+-- PHP verzió: 7.3.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Adatbázis: `f1`
+-- Adatbázis: `driver`
 --
 
 -- --------------------------------------------------------
@@ -46,17 +46,32 @@ INSERT INTO `shop` (`id`, `type`, `team`, `size`, `color`, `price`, `quantity`) 
 (2, 'T-shirt', 'Red-Bull', 'S', 'Kék', 20000, 20),
 (3, 'T-shirt', 'Red-Bull', 'M', 'Kék', 22000, 20),
 (4, 'T-shirt', 'Red-Bull', 'L', 'Kék', 25000, 20),
-(5, 'T-shirt', 'Red-Bull', 'XL', 'Kék', 25000, 20),
+(5, 'T-shirt', 'Red-Bull', 'XL', 'Kék', 28000, 20),
 (6, 'T-shirt', 'Red-Bull', 'XS', 'Fehér', 20000, 20),
 (7, 'T-shirt', 'Red-Bull', 'S', 'Fehér', 20000, 20),
 (8, 'T-shirt', 'Red-Bull', 'M', 'Fehér', 22000, 20),
 (9, 'T-shirt', 'Red-Bull', 'L', 'Fehér', 25000, 20),
-(10, 'T-shirt', 'Red-Bull', 'XL', 'Fehér', 25000, 20),
+(10, 'T-shirt', 'Red-Bull', 'XL', 'Fehér', 28000, 20),
 (11, 'T-shirt', 'Red-Bull', 'XS', 'Narancs', 20000, 20),
 (12, 'T-shirt', 'Red-Bull', 'S', 'Narancs', 20000, 20),
 (13, 'T-shirt', 'Red-Bull', 'M', 'Narancs', 22000, 20),
 (14, 'T-shirt', 'Red-Bull', 'L', 'Narancs', 25000, 20),
-(15, 'T-shirt', 'Red-Bull', 'XL', 'Narancs', 25000, 20);
+(15, 'T-shirt', 'Red-Bull', 'XL', 'Narancs', 28000, 20),
+(16, 'T-shirt', 'Ferrari', 'XS', 'Piros', 20000, 20),
+(17, 'T-shirt', 'Ferrari', 'S', 'Piros', 20000, 20),
+(18, 'T-shirt', 'Ferrari', 'M', 'Piros', 22000, 20),
+(19, 'T-shirt', 'Ferrari', 'L', 'Piros', 25000, 20),
+(20, 'T-shirt', 'Ferrari', 'XL', 'Piros', 28000, 20),
+(21, 'T-shirt', 'Ferrari', 'XS', 'Fekete', 20000, 20),
+(22, 'T-shirt', 'Ferrari', 'S', 'Fekete', 20000, 20),
+(23, 'T-shirt', 'Ferrari', 'M', 'Fekete', 22000, 20),
+(24, 'T-shirt', 'Ferrari', 'L', 'Fekete', 25000, 20),
+(25, 'T-shirt', 'Ferrari', 'XL', 'Fekete', 28000, 20),
+(26, 'T-shirt', 'Ferrari', 'XS', 'Fehér', 20000, 20),
+(27, 'T-shirt', 'Ferrari', 'S', 'Fehér', 20000, 20),
+(28, 'T-shirt', 'Ferrari', 'M', 'Fehér', 22000, 20),
+(29, 'T-shirt', 'Ferrari', 'L', 'Fehér', 25000, 20),
+(30, 'T-shirt', 'Ferrari', 'XL', 'Fehér', 28000, 20);
 
 --
 -- Indexek a kiírt táblákhoz
@@ -76,7 +91,7 @@ ALTER TABLE `shop`
 -- AUTO_INCREMENT a táblához `shop`
 --
 ALTER TABLE `shop`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
