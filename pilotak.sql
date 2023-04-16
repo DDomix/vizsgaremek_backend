084975688035b2cd29f8498c3cff9881c9d0958d
Fuzi18 <fuzesi.t2003@gmail.com>
2023-04-16 12:40:41 +0200

    pilotak sql

--- pilotak.sql
+++ pilotak.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2023. Ápr 14. 11:08
--- Kiszolgáló verziója: 10.4.24-MariaDB
--- PHP verzió: 8.1.6
+-- Létrehozás ideje: 2023. Ápr 16. 12:39
+-- Kiszolgáló verziója: 10.4.18-MariaDB
+-- PHP verzió: 7.3.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -49,7 +49,69 @@ INSERT INTO `pilotak` (`nev`, `kor`, `nemzetiseg`, `csapat`, `szerzettpontok`, `
 ('Logan Sargeant', 22, 'USA', 'Carlin', 148, 'Formula2', 4, 4),
 ('Ayumu Iwasa', 21, 'Japan', 'DAMS', 139, 'Formula2', 5, 5),
 ('Jack Doohan', 20, 'Australia', 'Virtuosi Racing', 128, 'Formula2', 6, 6),
-('Jehan Daruvala', 24, 'India', 'Prema Racing', 126, 'Formula2', 7, 7);
+('Jehan Daruvala', 24, 'India', 'Prema Racing', 126, 'Formula2', 7, 7),
+('Enzo Fittipaldi', 21, 'Brasil', 'Charouz Racing System', 126, 'Formula2', 7, 8),
+('Frederik Vesti', 21, 'Denmark', 'ART Grand Prix', 117, 'Formula2', 8, 9),
+('Dennis Hauger', 19, 'Norway', 'Prema Racing', 115, 'Formula2', 9, 10),
+('Juri Vips', 22, 'Estonia', 'Hitech Grand Prix', 114, 'Formula2', 10, 11),
+('Richard Verschoor', 22, 'Netherland', 'Trident', 103, 'Formula2', 11, 12),
+('Marcus Armstrong', 22, 'Australia', 'Hitech Grand Prix', 93, 'Formula2', 12, 13),
+('Clement Novalak', 22, 'France', 'MP Motorsport', 40, 'Formula2', 13, 14),
+('Ralph Boschung', 25, 'Switzerland', 'Campos Racing', 40, 'Formula2', 13, 15),
+('Jake Hughes', 28, 'United Kingdom', 'Van Amersfoort Racing', 26, 'Formula2', 14, 16),
+('Amaury Cordeel', 20, 'Belgium', 'Van Amersfoort Racing', 26, 'Formula2', 14, 17),
+('David Beckmann', 22, 'Germany', 'Van Amersfoort Racing', 25, 'Formula2', 15, 18),
+('Roy Nissany', 28, 'Israel', 'DAMS', 20, 'Formula2', 16, 19),
+('Roberto Merhi', 31, 'Spain', 'Campos Racing', 15, 'Formula2', 17, 20),
+('Olivér Caldwell', 20, 'United Kingdom', 'Campos Racing', 12, 'Formula2', 18, 21),
+('Marino Sato', 23, 'Japan', 'Virtuosi', 6, 'Formula2', 19, 22),
+('Calan Williams', 22, 'Australia', 'Trident', 5, 'Formula2', 20, 23),
+('Cem Bolukbasi', 25, 'Turkey', 'Charouz Racing System', 0, 'Formula2', 21, 24),
+('Luca Ghiotto', 28, 'Italy', 'Hitech Grand Prix', 0, 'Formula2', 21, 25),
+('Zane Maloney', 19, 'Barbados', 'Trident', 0, 'Formula2', 21, 26),
+('Juan Manuel Correa', 23, 'USA', 'ART Grand Prix', 0, 'Formula2', 21, 27),
+('Tatiana Calderon', 29, 'Columbia', 'Charouz Racing System', 0, 'Formula2', 21, 28),
+('Lirim Zendeli', 23, 'Germany', 'MP Motorsport', 0, 'Formula2', 21, 29),
+('Victor Martins', 21, 'France', 'ART Grand Prix', 139, 'Formula3', 1, 30),
+('Zane Maloney', 19, 'Barbados', 'Trident', 134, 'Formula3', 2, 31),
+('Oliver Bearman', 19, 'United Kingdom', 'Prema Racing', 132, 'Formula3', 3, 32),
+('Isack Hadjar', 18, 'France', 'Hitech Grand Prix', 123, 'Formula3', 4, 33),
+('Roman Stanek', 19, 'Czech Republic', 'Trident', 117, 'Formula3', 5, 34),
+('Arthur Leclerc', 22, 'Monaco', 'Prema Racing', 114, 'Formula3', 6, 35),
+('Jak Crawford', 17, 'USA', 'Prema Racing', 109, 'Formula3', 7, 36),
+('Caio Collet', 20, 'Brasil', 'MP Motorsport', 88, 'Formula3', 8, 37),
+('Franco Colapinto', 19, 'Argentina', 'Van Amersfoort Racing', 76, 'Formula3', 9, 38),
+('Alekszandr Szmoljar', 21, 'Russian', 'MP Motorsport', 76, 'Formula3', 9, 39),
+('Zak O\'Sullivan', 18, 'United Kingdom', 'Carlin', 54, 'Formula3', 10, 40),
+('Jonny Edgar', 19, 'United Kingdom', 'Trident', 46, 'Formula3', 11, 41),
+('Juan Manuel Correa', 23, 'USA', 'ART Grand Prix', 39, 'Formula3', 12, 42),
+('Kush Maini', 22, 'India', 'MP Motorsport', 31, 'Formula3', 13, 43),
+('Grégoire Saucy', 23, 'Switzerland', 'ART Grand Prix', 30, 'Formula3', 14, 44),
+('David Vidales', 20, 'Spain', 'Campos Racing', 29, 'Formula3', 15, 45),
+('Kaylen Federick', 20, 'USA', 'Hitech Grand Prix', 27, 'Formula3', 16, 46),
+('William Alatalo', 20, 'Finland', 'Jenzer Motorsport', 24, 'Formula3', 17, 47),
+('Oliver Goathe', 18, 'Monaco', 'Campos Racing', 15, 'Formula3', 18, 48),
+('Reece Ushijima', 20, 'United Kingdom', 'Van Amersfoort Racing', 13, 'Formula3', 19, 49),
+('Sebastian Montoya', 17, 'Columbia', 'Campos Racing', 7, 'Formula3', 20, 50),
+('Oliver Rasmussen', 2, 'Denmark', 'Trident', 4, 'Formula3', 21, 51),
+('Brad Benavides', 21, 'USA', 'Carlin', 3, 'Formula3', 22, 52),
+('Rafael Villagómez', 21, 'Mexico', 'Van Amersfoort Racing', 2, 'Formula3', 23, 53),
+('Pepe Martí', 17, 'Spain', 'Campos Racing', 2, 'Formula3', 23, 54),
+('Ido Cohen', 21, 'Israel', 'Jenzer Motorsport', 2, 'Formula3', 23, 55),
+('Francesco Pizzi', 18, 'Italy', 'Charouz Racing System', 1, 'Formula3', 24, 56),
+('David Schumacher', 21, 'Germany', 'Charouz Racing System', 0, 'Formula3', 25, 57),
+('Lirim Zendeli', 23, 'Germany', 'Charouz Racing System', 0, 'Formula3', 25, 58),
+('Ayrton Simmons', 21, 'United Kingdom', 'Charouz Racing System', 0, 'Formula3', 25, 59),
+('Tóth László', 22, 'Hungary', 'Charouz Racing System', 0, 'Formula3', 25, 60),
+('Christian Mansell', 18, 'Australia', 'Charouz Racing System', 0, 'Formula3', 25, 61),
+('Alessandro Famularo', 20, 'Italy', 'Charouz Racing System', 0, 'Formula3', 25, 62),
+('Zdenek Chovanec', 18, 'Portugal', 'Charouz Racing System', 0, 'Formula3', 25, 63),
+('Federico Malvestiti', 22, 'Italy', 'Jenzer Motorsport', 0, 'Formula3', 25, 64),
+('Niko Kari', 23, 'Finland', 'Jenzer Motorsport', 0, 'Formula3', 25, 65),
+('Nazim Azman', 21, 'Malaysia', 'Hitech Grand Prix', 0, 'Formula3', 25, 66),
+('Hunter Yeany', 17, 'USA', 'Campos Racing', 0, 'Formula3', 25, 67),
+('Enzo Trulli', 17, 'Italy', 'Carlin', 0, 'Formula3', 25, 68),
+('Filip Ugran', 20, 'Romania', 'MP Motorsport', 0, 'Formula3', 25, 69);
 
 --
 -- Indexek a kiírt táblákhoz
@@ -69,7 +131,7 @@ ALTER TABLE `pilotak`
 -- AUTO_INCREMENT a táblához `pilotak`
 --
 ALTER TABLE `pilotak`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
